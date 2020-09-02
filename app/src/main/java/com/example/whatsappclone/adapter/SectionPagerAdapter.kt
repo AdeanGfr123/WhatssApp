7f9205c12ee67b0dd105cfe27b6a379300182644
adeangafrio <adean6749@gmail.com>
2020-09-02 20:09:44 +0700

    WhatsApp

---
+++ app/src/main/java/com/example/whatsappclone/adapter/SectionPagerAdapter.kt
@@ -0,0 +1,19 @@
+package com.example.whatsappclone.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.whatsappclone.MainActivity
+
+class SectionPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+    return MainActivity.PlaceHolderFragment.newIntent( position + 1)
+
+    }
+
+    override fun getCount(): Int { return 3
+    }
+
+
+}
